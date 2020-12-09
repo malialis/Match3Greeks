d130c6e4dbad2977334520c33c63220e7635f075
nick malialis <batnick@gmail.com>
2020-12-09 13:26:09 -0700

    singleton scripts and start of soundmanager
    start of the sound manager yo

--- Assets/Scripts/Singleton.cs
+++ Assets/Scripts/Singleton.cs
@@ -2,17 +2,40 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Singleton : MonoBehaviour
+public class Singleton <T> : MonoBehaviour where T: MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    static T m_instance;
+
+    public static T Instance
     {
-        
+        get
+        {
+            if (m_instance == null)
+            {
+                m_instance = GameObject.FindObjectOfType<T>();
+                if(m_instance == null)
+                {
+                    GameObject singleton = new GameObject(typeof(T).Name);
+                    m_instance = singleton.AddComponent<T>();
+                }
+            }
+            return m_instance;
+        }        
     }
 
-    // Update is called once per frame
-    void Update()
+    public virtual void Awake()
     {
-        
+        if (m_instance == null)
+        {
+            m_instance = this as T;
+            transform.parent = null;
+            DontDestroyOnLoad(this.gameObject);
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
     }
+
+
 }
