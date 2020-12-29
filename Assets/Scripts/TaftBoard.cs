a6c08c3f63cba7a7a4c2b992c713ceb0534b338e
nick malialis <batnick@gmail.com>
2020-12-29 10:01:05 -0700

    Redux part 1
    redux

---
+++ Assets/Scripts/TaftBoard.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TaftBoard : MonoBehaviour
+{
+
+    public int width;
+    public int height;
+    public GameObject tilePrefab;
+
+    private BackgroundTile[,] allTiles;
+
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        allTiles = new BackgroundTile[width, height]; // tells the board how big it is going to be, not filling it in but size
+        SetUp();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void SetUp()
+    {
+        for(int i = 0; i < width; i++)
+        {
+            for(int j = 0; j < height; j++)
+            {
+                Vector2 tempPosition = new Vector2 (i, j);
+                GameObject backgroundTile = Instantiate(tilePrefab, tempPosition , Quaternion.identity) as GameObject; // populates board with the tilePrefab
+                backgroundTile.transform.parent = this.transform; // setting its parent to the board
+                backgroundTile.name = "( " + i + ", " + j + " )"; // naming it as its coordinates
+            }
+        }
+    }
+
+
+}
